861bf7c23d9965c6c70a4ccfd44d560a5273b908
Naimul Islam <naiimullislam@gmail.com>
2026-09-17 12:28:17 -0700

    Refactor motion button click handler for permissions
    Updated handleClick function to request motion permissions and handle errors.

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
 const motionButton = document.getElementById("enableMotion");
+const motionStatus = document.getElementById("motionStatus");
+
 let hasStarted = false;
-function handleClick() {
+async function handleClick() {
     if (hasStarted) {
         return;
     }
-    hasStarted = true;
-    motionButton.textContent = "Ready to bowl!";
-    motionStatus.textContent = "Button clicked. Motion controls are not enabled yet.";
-    motionButton.animate(
-        [   {boxShadow: "0 0 5px blue"},
-            {boxShadow: "0 0 20px pink"},
-            {boxShadow: "0 0 20px blue"},
-            {boxShadow: "0 0 5px pink"},
-            {boxShadow: "0 0 5px blue"}
-        ],
-        {
+
+    // catch error block
+    try {
+        if (
+            typeof DeviceMotionEvent === "undefined" ||
+            typeof DeviceMotionEvent.requestPermission !== "function"
+        ) {
+            motionStatus.textContent = "For this test, open the page in Safari on your iPhone.";
+            return;
+        }
+        const permission = await DeviceMotionEvent.requestPermission();
+        if (permission !== "granted") {
+            motionStatus.textContent = " Motion permission was not granted.";
+            return;
+        } else {
+            motionStatus.textContent = "Permission granted. Motion readings are not connected yet.";
+        }
+        // Intructions that might fail go here
+        hasStarted = true;
+        motionButton.textContent = "Ready to bowl!";
+        motionButton.style.boxShadow = "0 0 20px blue";
+        motionButton.animate(
+            [   {boxShadow: "0 0 5px blue"},
+                {boxShadow: "0 0 20px pink"},
+                {boxShadow: "0 0 20px blue"},
+                {boxShadow: "0 0 5px pink"},
+                {boxShadow: "0 0 5px blue"}
+            ],
+            {
              duration: 1200,
              iterations: Infinity
-        }
-    )
+            }
+        );
+    } catch (error) {
+        // Instructions for handling a failure go here
+        hasStarted = false;
+        motionStatus.textContent = "Something went wrong. Please try again.";
+    }
+
 }
 motionButton.addEventListener("click", handleClick);
 
-const motionStatus = document.getElementById("motionStatus");
+
 
 
